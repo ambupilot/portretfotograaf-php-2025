ff4e25525c8a3fd1b438d973baa906631232ee3c
Martijn Kerssing <martijn@kerssing.nl>
2025-06-05 10:55:09 +0200

    .env added

--- classes/Database.php
+++ classes/Database.php
@@ -1,13 +1,15 @@
 <?php
+require_once __DIR__ . '/../env.php';
+
 class Database {
     private static $conn;
 
     public static function getConnection() {
         if (!self::$conn) {
-            $host = 'localhost';
-            $db = 'portretfotograaf';
-            $user = 'martijn';
-            $pass = 'kerssing';
+            $host = $_ENV['DB_HOST'];
+            $db = $_ENV['DB_NAME'];
+            $user = $_ENV['DB_USER'];
+            $pass = $_ENV['DB_PASS'];
             $dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
             self::$conn = new PDO($dsn, $user, $pass, [
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
---
+++ env.php
@@ -0,0 +1,13 @@
+<?php
+// Eenvoudige .env loader
+//$envPath = __DIR__ . '/../.env'; // LET OP; bij produktie .env in onderliggende map plaatsen!
+$envPath = __DIR__ . '/.env';
+if (file_exists($envPath)) {
+    $lines = file($envPath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    foreach ($lines as $line) {
+        if (strpos(trim($line), '#') === 0) continue;
+        list($key, $value) = explode('=', $line, 2);
+        $_ENV[trim($key)] = trim($value);
+    }
+}
+?>
